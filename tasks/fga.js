defb2cb0ccbe9c984ea338c19ca933a04ae57a85
Mike H. Hawley <mikehhawley@gmail.com>
2013-03-22 16:49:37 +0400

    fga first version

--- tasks/fga.js
+++ tasks/fga.js
@@ -1,29 +1,47 @@
-/**
- * {@link http://fucking-great-advice.ru/}
- */
+// ———————————————————————————————————————
 
-//utf8 to 1251 converter (1 byte format, RU/EN support only + any other symbols) by drgluck
-function utf8_decode (aa) {
-    var bb = '', c = 0;
-    for (var i = 0; i < aa.length; i++) {
-        c = aa.charCodeAt(i);
-        if (c > 127) {
-            if (c > 1024) {
-                if (c == 1025) {
-                    c = 1016;
-                } else if (c == 1105) {
-                    c = 1032;
-                }
-                bb += String.fromCharCode(c - 848);
-            }
-        } else {
-            bb += aa.charAt(i);
-        }
+exports.info = [
+    {
+        lang : 'ru',
+        command : 'обс',
+        signature : '!обс [<тэг>]',
+        description : 'Возвращает случайный совет'
+    },
+
+    {
+        lang : 'en',
+        command : 'fga',
+        signature : '!fga [<tag>]',
+        description : 'Returns random advice'
     }
-    return bb;
-}
+];
 
-exports.command = 'обс';
-exports.execute = function(body, cb) {
-	cb(null, String.fromCharCode(129));
+// ———————————————————————————————————————
+
+http = require('http');
+
+// ———————————————————————————————————————
+
+url = 'http://fucking-great-advice.ru/';
+
+// ———————————————————————————————————————
+
+exports.execute = function(body, callback) {
+    if (body.length)
+        method = 'api/random_by_tag/'+body;
+    else
+        method = 'api/random';
+
+    http.get(url+method, function (response) {
+        response.on('data', function(chunk) {
+            try
+                result = JSON.parse(chunk).text;
+            catch (error)
+                callback(error);
+
+            callback(null, advice.text);
+        });
+    });
 };
+
+// ———————————————————————————————————————
