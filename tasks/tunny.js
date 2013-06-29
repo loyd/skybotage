c6419a0612d62c07998f8ec9914c616d0fa6ffbe
Paul Loyd <pavelko95@gmail.com>
2013-06-29 16:40:12 +0400

    Add tunny task

---
+++ tasks/tunny.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var LURKMORE_URL = 'http://lurkmore.to/Смехуечки:Сосни_хуйца';
+var request = require('request');
+
+exports.info = {
+    ru : {
+          command : 'тунца'
+        , signature : '!тунца'
+        , description : 'Возвращает случайную смехуечку с lurkmore.to'
+    }
+}
+
+var next = (function(store) {
+    var tagReg = /<.*?>/g;
+
+    function shuffler() {
+        return .5 - Math.random();
+    }
+
+    return function(answer) {
+        if(store.length) return answer(store.pop());
+
+        request(LURKMORE_URL, function(err, res, body) {
+            if(err) console.error(err);
+
+            var start = body.indexOf('<ol>') + 4
+              , end   = body.indexOf('</ol>');
+
+            store = body
+                .slice(start, end)
+                .split('\n')
+                .map(function(tip) {
+                    return tip.replace(/<.*?>/g, '').trim();
+                })
+                .filter(String)
+                .sort(shuffler);
+
+            answer(store.pop());
+        });
+    }
+})([]);
+
+exports.execute = function(data, answer) {
+    next(answer);
+};
